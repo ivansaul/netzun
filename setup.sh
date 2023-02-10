ac65ba3f65dcc8dab6a858b78e685d2b967d731b
ivansaul <saul_2503@hotmail.com>
2023-02-10 02:33:06 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-# Install chromium
+# Install chromium / driver
 sudo apt update
-sudo apt install chromium-browser
+sudo apt install chromium-browser chromium-chromedriver -y
 
-# Install chromedriver
-# https://chromedriver.chromium.org/downloads
-wget https://chromedriver.storage.googleapis.com/110.0.5481.77/chromedriver_linux64.zip
-unzip chromedriver*
-sudo chmod +x chromedriver
-sudo cp chromedriver /usr/bin/chromedriver
-
-# Install firefox
+# Install firefox driver
 sudo apt update
-sudo apt install firefox
-
-# Install geckodriver
-# 
-wget https://github.com/mozilla/geckodriver/releases/download/v0.32.2/geckodriver-v0.32.2-linux64.tar.gz
-tar -xvzf geckodriver*
-sudo chmod +x geckodriver
-sudo cp geckodriver /usr/local/bin/
+sudo apt install firefox firefox-geckodriver -y
